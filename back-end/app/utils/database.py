23e0b3d8395a875c0324ee5354e16242dd77929e
dgrimaldi <grimi.david@gmail.com>
2023-11-06 16:00:08 +0100

    refactor: rename variable to user colletcion

--- back-end/app/utils/database.py
+++ back-end/app/utils/database.py
@@ -8,7 +8,7 @@ from pydantic.functional_validators import BeforeValidator
 client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
 db = client.get_database("college")
 chat_collection = db.get_collection("chats")
-users_collection = db.get_collection("users")
+user_collection = db.get_collection("users")
 
 # Represents an ObjectId field in the database.
 # It will be represented as a `str` on the model so that it can be serialized to JSON.
